ec22b6eb8199844c0477276b65dc1092d05c715c
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-05-28 15:23:55 +0000

    so minor changes

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
   let result = '';
 
-  // Iterate through the generator object
   for (const employee of reportWithIterator) {
-    // Append each employee name to the result string, separated by '|'
     result += `${employee} | `;
   }
-
-  // Remove the trailing '|' character and any whitespace
   return result.trimEnd().slice(0, -1);
 
 }
