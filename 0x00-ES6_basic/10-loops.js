fa4346bf2b2348252f83bfef2b1418d424365b89
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-05-28 14:24:51 +0000

    some minor changes

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, value] of array.entries()) {
-    array[idx] = appendString + value;
+  const resultArray = [];
+  for (const value of array) {
+    resultArray.push(appendString + value);
   }
-  return array;
+  return resultArray;
 }
