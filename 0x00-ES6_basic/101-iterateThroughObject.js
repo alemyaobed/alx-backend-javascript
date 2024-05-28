431f4ffb8a6b54bed667383e677dfd751a17de4a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-05-28 15:36:15 +0000

    some changes

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-
-  for (const employee of reportWithIterator) {
-    result += `${employee} | `;
-  }
-  return result.trimEnd().slice(0, -1);
-
+  return [...reportWithIterator].join(' | ');
 }
