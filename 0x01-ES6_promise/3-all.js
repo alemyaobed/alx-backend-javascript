256ce92eece60b7d49fc636983c987c49069e284
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-05-30 15:52:51 +0000

    some minor changes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,17 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  uploadPhoto().then((response) => {
-    console.log(response.body);
-  }).catch(() => {
-    console.error('Signup system offline');
-  });
-  createUser().then((response) => {
-    console.log(response.firstName);
-    console.log(response.lastName);
-  }).catch(() => {
-    console.error('Signup system offline');
-  });
+  Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      console.log(results[0].body, results[1].firstName, results[1].lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
