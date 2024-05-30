3c056397b223262747cc04e22082d17b9d54ce4c
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-05-30 16:45:01 +0000

    some minor changes

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,23 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
-}
+export default (firstName, lastName, fileName) => {
+  const promises = [signUpUser(firstName, lastName),
+    uploadPhoto(fileName)];
+
+  return Promise.allSettled(promises)
+    .then((values) => {
+      const res = [];
+      values.forEach((value) => {
+        if (value.status === 'fulfilled') {
+          res.push(value);
+        } else {
+          res.push({
+            status: value.status,
+            value: `Error: ${value.reason.message}`,
+          });
+        }
+      });
+      return res;
+    });
+};
