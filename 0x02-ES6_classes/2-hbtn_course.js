6ef581037521a92c7b74fba24b4fbc218eddc076
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-05-31 09:51:03 +0000

    some major changes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,37 +1,43 @@
 export default class HolbertonCourse {
-  constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
-  }
-  set name(name) {
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
-      throw new TypeError('Name must be a string');
+    constructor(name, length, students) {
+        this._name = name;
+        this._length = length;
+        this._students = students;
     }
-  }
-  get name() {
-    return this._name;
-  }
-  set length(length){
-    if (isNaN(length)) {
-      throw new TypeError('Length must be a number')
-    } else {
-      this._length = length;
+    
+    get name() {
+        return this._name;
     }
-  }
-  get length() {
-    return this._length;
-  }
-  set students(students) {
-    if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
-      this._students = students;
-    } else {
-      throw new TypeError('Students must be an array of strings');
+    
+    set name(value) {
+        if (typeof value === 'string') {
+        this._name = value;
+        } else {
+        throw new TypeError('Name must be a string');
+        }
     }
-  }
-  get students() {
-    return this._students;
-  }
+    
+    get length() {
+        return this._length;
+    }
+    
+    set length(value) {
+        if (typeof value === 'number') {
+        this._length = value;
+        } else {
+        throw new TypeError('Length must be a number');
+        }
+    }
+    
+    get students() {
+        return this._students;
+    }
+    
+    set students(value) {
+        if (Array.isArray(value) && value.every((v) => typeof v === 'string')) {
+        this._students = value;
+        } else {
+        throw new TypeError('Students must be an array of strings');
+        }
+    }
 }
