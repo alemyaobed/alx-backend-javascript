6047e115536aeebb917a1609d7dbb10a8ab12de7
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-05-31 19:02:08 +0000

    implementation of task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be as string')
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string')
+    }
+    this._name = name;
+    this._code = code;
+  }
+  get name() {
+    return this._name;
+  }
+  get code() {
+    return this._code;
+  }
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('name must be a string')
+    }
+    this._name;
+    }
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('code must be as string')
+    }
+    this._code;
+  }
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
---
+++ 0x02-ES6_classes/try.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def lemy(name):
+    print('hi ', name)
+
+name = lemy('lemy')
+
+rice = 57
+
+rice = 'sname'
+
+print(rice)
