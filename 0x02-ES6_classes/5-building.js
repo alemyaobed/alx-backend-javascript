20439066926b2405bd0c47938f1a945eae74c48e
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-01 04:34:04 +0000

    implementation of task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,20 @@
-
-// Consider this class as an abstract class. And make sure that any class that extends from it should implement a method named evacuationWarningMessage.
-// If a class that extends from it does not have a evacuationWarningMessage method, throw an error with the message Class extending Building must override evacuationWarningMessage
-
 export default class Building {
-    constructor(sqft) {
-        if (typeof sqft !== 'number') {
-            throw new TypeError('sqft must be a number');
-        }
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
-    get sqft() {
-        return this._sqft;
+    if ((this.evacuationWarningMessage === undefined) && (this.constructor !== Building)) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    set sqft(value) {
-        if (typeof sqft !== 'number') {
-            throw new TypeError('sqft must be a number');
-        }
-        this._sqft = value;
+    this._sqft = sqft;
+  }
+  get sqft() {
+    return this._sqft;
+  }
+  set sqft(value) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
     }
+    this._sqft = value;
+  }
 }
