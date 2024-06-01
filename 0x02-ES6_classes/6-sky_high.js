66ac7bed3f86de59dd49fcc504ec0e351f8c26d8
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-01 05:38:05 +0000

    changes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,5 +1,29 @@
 import Building from './5-building';
 
+import Building from './5-building';
+
+// export default class SkyHighBuilding extends Building {
+//     constructor(sqft, floors) {
+//         super(sqft);
+//         if (typeof floors !== 'number') {
+//             throw new TypeError('floors must be a number');
+//         }
+//         this._floors = floors;
+//     }
+//     get floors() {
+//         return this._floors;
+//     }
+//     set floors(value) {
+//         if (typeof value !== 'number') {
+//             throw new TypeError('floors must be a number');
+//         }
+//         this._floors = value;
+//     }
+//     evacuationWarningMessage() {
+//         return `Evacuate slowly the ${this.floors} floors.`
+//     }
+// }
+
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
