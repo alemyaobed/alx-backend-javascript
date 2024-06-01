458707e3578f7513d2c3f9ef242dad1a90a593b5
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-01 05:27:56 +0000

    some minor changes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,22 +1,11 @@
-// Import Building from 5-building.js.
-
-// Implement a class named SkyHighBuilding that extends from Building:
-
-// Constructor attributes:
-// sqft (Number) (must be assigned to the parent class Building)
-// floors (Number)
-// Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
-// Implement a getter for each attribute.
-// Override the method named evacuationWarningMessage and return the following string Evacuate slowly the NUMBER_OF_FLOORS floors.
-
-import Building from './5-building';
+import Building from './5-building.js';
 
 export default class SkyHighBuilding extends Building {
     constructor(sqft, floors) {
+        super(sqft);
         if (typeof floors !== 'number') {
             throw new TypeError('floors must be a number');
         }
-        super(sqft);
         this._floors = floors;
     }
     get floors() {
