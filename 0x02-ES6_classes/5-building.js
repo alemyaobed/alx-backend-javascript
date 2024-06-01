d3c0e89e1b532cf5a5c2e6fc2362b4e4b22cb764
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-01 05:41:20 +0000

    changes 1

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,25 +1,37 @@
+// export default class Building {
+//   constructor(sqft) {
+//     if (typeof sqft !== "number") {
+//       throw new TypeError("sqft must be a number");
+//     }
+//     if (
+//       this.evacuationWarningMessage === undefined &&
+//       this.constructor !== Building
+//     ) {
+//       throw new Error(
+//         "Class extending Building must override evacuationWarningMessage"
+//       );
+//     }
+//     this._sqft = sqft;
+//   }
+//   get sqft() {
+//     return this._sqft;
+//   }
+//   set sqft(value) {
+//     if (typeof value !== "number") {
+//       throw new TypeError("sqft must be a number");
+//     }
+//     this._sqft = value;
+//   }
+// }
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== "number") {
-      throw new TypeError("sqft must be a number");
-    }
-    if (
-      this.evacuationWarningMessage === undefined &&
-      this.constructor !== Building
-    ) {
-      throw new Error(
-        "Class extending Building must override evacuationWarningMessage"
-      );
-    }
     this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
+
   get sqft() {
     return this._sqft;
-  }
-  set sqft(value) {
-    if (typeof value !== "number") {
-      throw new TypeError("sqft must be a number");
-    }
-    this._sqft = value;
   }
 }
