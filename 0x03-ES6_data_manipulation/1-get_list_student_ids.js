19bbecca6a9e0ba6057fc24a9893f0acdd98336d
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-04 17:03:01 +0000

    some minor changes

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,12 +1,8 @@
 // Create a function getListStudentIds that returns an array of ids from a list of object.
-
 // This function is taking one argument which is an array of objects - and this array is the same
 // format as getListStudents from the previous task.
-
 // If the argument is not an array, the function is returning an empty array.
-
 // You must use the map function on the array.
-
 export default function getListStudentIds(studentList) {
   const result = [];
   if (Array.isArray(studentList)) {
