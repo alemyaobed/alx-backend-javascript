e3526017b6009e97519885ea9a8968a5e410b485
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-04 17:31:09 +0000

    implementation of task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,15 +1,13 @@
-// Create a function getStudentsByLocation that returns an array of objects who are located in a specific city.
+// Create a function getStudentsByLocation that returns an array of
+// objects who are located in a specific city.
 
 // It should accept a list of students (from getListStudents) and a city (string) as parameters.
 
 // You must use the filter function on the array.
 
-export default function getStudentsByLocation(studentList) {
-  const result = [];
-  if (Array.isArray(studentList)) {
-    for (const object of studentList) {
-      result.push(object.id);
-    }
+export default function getStudentsByLocation(studentList, city) {
+  if (Array.isArray(studentList) && typeof city === 'string') {
+    return studentList.filter((student) => student.location === city);
   }
-  return result;
+  return [];
 }
