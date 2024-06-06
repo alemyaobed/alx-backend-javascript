c6aea11c54deb95549e74a27cc6b071a65daf681
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-06-06 01:04:17 +0000

    implementation of task 5

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,64 @@
+// The DirectorInterface interface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// The TeacherInterface interface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// The Director class implementing the DirectorInterface interface
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home'
+  }
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break'
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks'
+  }
+}
+
+// The Teacher class implementing the TeacherInterface interface
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+// Create a function createEmployee with the following requirements:
+// It can return either a Director or a Teacher instance
+// It accepts 1 arguments:
+// salary(either number or string)
+// if salary is a number and less than 500 - It should return a new Teacher. Otherwise it should return a Director
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
+
+// Example usage
+console.log(createEmployee(200)); // Output: Teacher
+console.log(createEmployee(1000)); // Output: Director
+console.log(createEmployee('$500')); // Output: Director
+
+// For detailed output:
+const employee1 = createEmployee(200);
+console.log(employee1.workFromHome()); // Output: Cannot work from home
+
+const employee2 = createEmployee(1000);
+console.log(employee2.workFromHome()); // Output: Working from home
