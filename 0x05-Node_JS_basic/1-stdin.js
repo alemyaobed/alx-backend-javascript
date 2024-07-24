8bc43db01ab5149a52d8e2fdd3d9395f3f18774a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-24 10:27:11 +0000

    some corrections

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,27 +1,12 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create an interface for input and output
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-// Display the initial message to the user
-console.log('Welcome to Holberton School, what is your name?');
-
-// Handle the input from the user
-rl.on('line', (input) => {
-  // Display the user's name
-  console.log(`Your name is: ${input}`);
-});
-
-// Handle the close event
-rl.on('close', () => {
-  // Display the closing message
-  console.log('This important software is now closing');
-});
-
-// Handle the SIGINT event (Ctrl+C)
-process.on('SIGINT', () => {
-  rl.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
