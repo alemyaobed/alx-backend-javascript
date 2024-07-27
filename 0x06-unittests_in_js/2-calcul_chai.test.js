6073371db9d2e1c13f099e53fddee1798cc57efe
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-07-27 12:25:58 +0000

    some changes

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -7,8 +7,8 @@ describe('calculateNumber', () => {
     it('should return 6 when inputs are 1.4 and 4.5', () => {
       expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
-    it('should return 5 when inputs are 1.5 and 3.5', () => {
-      expect(calculateNumber('SUM', 1.5, 3.5)).to.equal(5);
+    it('should return 6 when inputs are 1.5 and 3.5', () => {
+      expect(calculateNumber('SUM', 1.5, 3.5)).to.equal(6);
     });
   });
 
@@ -25,8 +25,8 @@ describe('calculateNumber', () => {
     it('should return 0.2 when inputs are 1.4 and 4.5', () => {
       expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
-    it('should return 1 when inputs are 2.5 and 2', () => {
-      expect(calculateNumber('DIVIDE', 2.5, 2)).to.equal(1);
+    it('should return 1.5 when inputs are 2.5 and 2', () => {
+      expect(calculateNumber('DIVIDE', 2.5, 2)).to.equal(1.5);
     });
     it('should return "Error" when divisor is 0', () => {
       expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
